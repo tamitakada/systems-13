24e62c8676dbbde3ff64d2799cc021498103340d
tamitakada <tamitakada@gmail.com>
2021-11-06 22:22:51 -0400

    better testing

--- main.c
+++ main.c
@@ -2,5 +2,13 @@
 #include <stdio.h>
 
 int main() {
-  read_csv("nyc_pop.csv");
+    read_csv("nyc_pop.csv");
+    
+    printf("Reading data first time ===================================\n");
+    read_data();
+    
+    add_data();
+    
+    printf("\nReading data with new entry ===================================\n");
+    read_data();
 }
