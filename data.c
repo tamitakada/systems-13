5215fe4c6e06ca8513a4f55e58ab7bc7b3791b21
tamitakada <tamitakada@gmail.com>
2021-11-06 22:09:46 -0400

    got read csv working, added read data

--- data.c
+++ data.c
@@ -11,60 +11,93 @@ struct pop_entry {
   char boro[15];
 };
 
-void read_csv(char *file_path) {
-  int csv_file = open(file_path, O_RDONLY, 0);
-  
-  struct stat stats;
-  stat(file_path, &stats);
-  int size = stats.st_size;
-  
-  if (csv_file >= 0) {
-    int bytes_read = 0;
-    int lines_read = 0;
+void read_data() {
+    struct stat stats;
+    stat("pop_data", &stats);
     
-    int index = 0;
-    char buffer;
-
-    char ***full_data = malloc(100);
-
-    while (bytes_read < size) {
-      char **all_data = malloc(100);
-      char data[100];
-
-      int a_index = 0;
-      int d_index = 0;
+    int len = stats.st_size / sizeof(struct pop_entry);
+    struct pop_entry entries[len];
     
-      while (buffer != '\n') {
-	if (buffer == ',') {
-	  data[d_index] = '\0';
-	  char *dest = malloc(d_index * sizeof(char));
-	  strcpy(dest, data);
-	  printf("%s\n", data);
-
-	  all_data[a_index] = dest;
-	
-	  a_index++;
-	  d_index = 0;
-	}
-      
-	bytes_read += read(csv_file, &buffer, sizeof(char));
-	printf("%c\n", buffer);
-	data[d_index] = buffer;
-	d_index++;
-      }
-
-      full_data[lines_read] = all_data;
-      lines_read++;
-    }
-
-    /*
+    int file = open("pop_data", O_RDONLY, 0);
+    
+    read(file, entries, stats.st_size);
+    
     int i;
-    for (i = 0; i < 10; i++) {
-      printf("%s\n", all_data[i]);
+    for (i = 0; i < len; i++) {
+        printf("Entry | Year: %d, Population: %d, Borough: %s\n", entries[i].year, entries[i].population, entries[i].boro);
     }
-    */
+}
+
+void write_file(struct pop_entry *entries, int len) {
+    int file = open("pop_data", O_CREAT | O_WRONLY, 0644);
+    write(file, entries, sizeof(struct pop_entry) * len);
+    close(file);
+}
+
+void read_csv(char *file_path) {
+    int csv_file = open(file_path, O_RDONLY, 0);
     
-    // int data_file = open("data.txt", O_CREAT | O_WRONLY, 0644);
-    // write(data_file, buffer, 100);
-  }
+    struct pop_entry all_entries[1000];
+    int index = 0;
+    
+    if (csv_file >= 0) {
+        char buffer;
+        int bytes_read = read(csv_file, &buffer, sizeof(char));
+        
+        char line[1000];
+        char l_index = 0;
+        
+        int lines_read = 0;
+        
+        char boro_names[5][100] = {
+            "Manhattan",
+            "Brooklyn",
+            "Queens",
+            "Bronx",
+            "Staten Island"
+        };
+        
+        while (bytes_read) {
+            if (buffer == '\n') {
+                if (lines_read > 0) {
+                    line[l_index] = '\0';
+   
+                    int year;
+                    int boro_pops[5];
+                    
+                    sscanf(
+                        line,
+                        "%d,%d,%d,%d,%d,%d",
+                        &year,
+                        &boro_pops[0],
+                        &boro_pops[1],
+                        &boro_pops[2],
+                        &boro_pops[3],
+                        &boro_pops[4]
+                    );
+                    
+                    int i;
+                    for (i = 0; i < 5; i++) {
+                        struct pop_entry pop;
+                        pop.year = year;
+                        pop.population = boro_pops[i];
+                        strcpy(pop.boro, boro_names[i]);
+                        all_entries[index] = pop;
+                        
+                        index++;
+                    }
+                }
+                
+                l_index = 0;
+                lines_read++;
+                bytes_read = read(csv_file, &buffer, sizeof(char));
+            } else {
+                line[l_index] = buffer;
+                l_index++;
+                bytes_read = read(csv_file, &buffer, sizeof(char));
+            }
+        }
+        
+        write_file(all_entries, index);
+    }
 }
--- data.h
+++ data.h
@@ -1,5 +1,6 @@
 #ifndef DATA
 
+void read_data();
 void read_csv(char *file_path);
 
 #endif
